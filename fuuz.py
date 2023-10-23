00360ddf516602358aa247d3d5a288a03454a6bc
Joao Pedro Ventura <joao-ventura55@hotmail.com>
2023-10-23 17:33:04 -0300

    Divisao de responsabilidades da resposta e separacao do banner

--- fuuz.py
+++ fuuz.py
@@ -2,6 +2,7 @@ import requests
 import sys
 from time import sleep
 
+from banner import get_banner
 from config import Config
 from exceptions import InvalidHTTPMethodError, InvalidUrlFormat, MissingFUZZTargetError
 from method_controller import MethodController
@@ -29,17 +30,8 @@ class Fuzzer:
         """
         Display the banner and configuration information.
         """
-        print('''
-"""""""""""""""""""""""""""""""""""+
- ________       _____  _____       "
-|_   __  |     |_   _||_   _|      "
-  | |_ \_|__   _ | |    | | ____   "
-  |  _|  [  | | || '    ' |[_   ]  "
- _| |_    | \_/ |,\ \__/ /  .' /_  "
-|_____|   '.__.'_/ `.__.'  [_____] "
-                                   "
-            @_@.-.                 "
-            <",_( )_~              "''')
+        banner = get_banner()
+        print(banner)
         print('v1 --------------------- Made By Pixel.Def')
         print('-' * 50, '\n')
         sleep(1)
@@ -99,27 +91,37 @@ class Fuzzer:
         
         self.request_method = self.request_method if not self.request_method is None else 'GET'
 
-        status_code = StatusCodeController(self.status_code)
-        selected_status_code = [int(selected_status_code) for selected_status_code in status_code.get_status_codes()]
+        status_code = self.get_status_code()
 
         response = requests.request(self.request_method, parsed_url)
         
+        self.response_controller(response, payload, status_code)
+
+    
+    def response_controller(self, response, payload, status_code):
         response_content = response.text
         response_status_code = response.status_code
         response_size = len(response.content)
         response_time = response.elapsed.total_seconds()
 
-        if (not self.content_reader is None and self.content_reader in response_content) and (response_status_code in selected_status_code):
+        if (not self.content_reader is None and self.content_reader in response_content) and (response_status_code in status_code):
             print(f'\r{payload.ljust(30)}           [Status: {response_status_code}, Size: {response_size}, Duration: {response_time}]')
         
-        elif response_status_code in selected_status_code and self.content_reader is None:
+        elif response_status_code in status_code and self.content_reader is None:
             print(f'\r{payload.ljust(30)}           [Status: {response_status_code}, Size: {response_size}, Duration: {response_time}]')
             
-        elif (not self.content_length is None and response_size >= self.content_length) and (response_status_code in selected_status_code):
+        elif (not self.content_length is None and response_size >= self.content_length) and (response_status_code in status_code):
             print(f'\r{payload.ljust(30)}           [Status: {response_status_code}, Size: {response_size}, Duration: {response_time}]')
             
         else:
             pass
+        
+        
+    def get_status_code(self):
+        status_code = StatusCodeController(self.status_code)
+        return [int(selected_status_code) for selected_status_code in status_code.get_status_codes()]
+        
+        
     
     def file_read(self):
         """
